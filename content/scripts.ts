f840825a8009f971e5e956d3a9fec424ed1a084e
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-09-14 20:52:35 +0900

    🔥 不要なコードを削除

--- content/scripts.ts
+++ content/scripts.ts
@@ -1,6 +1,4 @@
-import './styles.css'
-import {getContentHtml} from './content'
+import "./styles.css";
+import { getContentHtml } from "./content";
 
-console.log('hello from content_scripts')
-
-document.body.innerHTML += `<div id="extension-root">${getContentHtml()}</div>`
+document.body.innerHTML += `<div id="extension-root">${getContentHtml()}</div>`;
